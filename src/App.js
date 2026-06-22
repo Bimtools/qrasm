8f19d153b8e305a4650a387e9dee6f0c7a9bf6d1
Bimtools4.0 <Bimtools4.0@gmail.com>
2026-06-22 09:17:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -28,190 +28,147 @@ function App() {
   const [asm, setAsm] = useState();
   const [modelId, setModelId] = useState();
 
-  // async function fetchData() {
-  //   const tcapi = await WorkspaceAPI.connect(window.parent);
-  //   const token = await tcapi.extension.requestPermission("accesstoken");
-  //   window.localStorage.setItem("trimbleToken", token);
-  //   const url = window.location.href;
-  //   const propertyString = url.split("?")[1];
-  //   const ifcGuid = propertyString?.split("ibim")[0];
-  //   const fId = propertyString?.split("ibim")[1];
-  //   if (!ifcGuid) {
-  //     return;
-  //   }
-  //   if (ifcGuid.length !== 22) {
-  //     return;
-  //   }
-  //   if (!fId) {
-  //     return;
-  //   }
-  //   dispatch(
-  //     GetDrawingRequest({
-  //       id: fId,
-  //     }),
-  //   );
+  // function parseUrlParams() {
+  //   const [, query] = window.location.href.split("?");
+  //   if (!query) throw new Error("Missing URL params");
 
-  //   var models;
-  //   do {
-  //     models = await tcapi.viewer.getModels();
-  //   } while (models === undefined || models.length === 0);
-  //   var asm;
-  //   var modelId;
-  //   for (const model of models) {
-  //     const modelName = model.name;
-  //     if (modelName.indexOf(".trb") >= 0) {
-  //       console.log(modelName);
-  //       dispatch(
-  //         GetAnnIdRequest({
-  //           name: model.name,
-  //           modelId: model.id,
-  //         }),
-  //       );
-  //     }
-  //   }
-  //   await tcapi.viewer.reset();
-  //   for (const model of models) {
-  //     const modelName = model.name;
-  //     if (modelName.indexOf(".ifc") >= 0 || modelName.indexOf(".tekla") >= 0) {
-  //       const loadedModel = await tcapi.viewer.getLoadedModel(model.id);
-  //       console.log(loadedModel);
-  //       if (loadedModel === undefined) {
-  //         await tcapi.viewer.toggleModel(model.id, true, true);
-  //       }
+  //   const ifcGuid = query.split("ibim")[0];
+  //   const fileId = query.split("ibim")[1];
 
-  //       await tcapi.viewer.setCamera("reset");
-  //       var modelObjects;
-  //       do {
-  //         modelObjects = await tcapi.viewer.getObjects();
-  //       } while (modelObjects === undefined || modelObjects.length === 0);
+  //   if (!ifcGuid || ifcGuid.length !== 22)
+  //     throw new Error(`Invalid ifcGuid: "${ifcGuid}"`);
+  //   if (!fileId) throw new Error("Missing fileId");
 
-  //       const runtimeIds = await tcapi.viewer.convertToObjectRuntimeIds(
-  //         model.id,
-  //         [ifcGuid],
-  //       );
-  //       console.log("----------------------------------------------");
-  //       console.log(runtimeIds);
-
-  //       if (
-  //         runtimeIds !== undefined &&
-  //         runtimeIds.length > 0 &&
-  //         runtimeIds[0] >= 0
-  //       ) {
-  //         asm = runtimeIds[0];
-  //         modelId = model.id;
-  //         break;
-  //       }
-  //     }
-  //   }
-  //   setAsm(asm);
-  //   setModelId(modelId);
-  //   await tcapi.viewer.setSelection({
-  //     modelObjectIds: [
-  //       {
-  //         modelId: modelId,
-  //         objectRuntimeIds: [asm],
-  //       },
-  //     ],
-  //   });
-  //   await tcapi.viewer.isolateEntities([
-  //     {
-  //       modelId: modelId,
-  //       entityIds: [asm],
-  //     },
-  //   ]);
-  //   // await tcapi.viewer.setCamera({
-  //   //   position: {
-  //   //     x: 1358.0000001497558,
-  //   //     y: 2231.9649982910159,
-  //   //     z: 111.12399997144837,
-  //   //   },
-  //   //   projectionType: "ortho",
-  //   //   yaw: Math.PI,
-  //   //   pitch: 0,
-  //   // });
-  //   await tcapi.viewer.setCamera({
-  //     modelObjectIds: [
-  //       {
-  //         modelId: modelId,
-  //         objectRuntimeIds: [asm],
-  //       },
-  //     ],
-  //   });
-
-  //   do {
-  //     const tcapi = await WorkspaceAPI.connect(window.parent);
-  //     const annObjs = annIds.find((x) => x.name === views[0]?.file);
-  //     console.log(annIds);
-  //     console.log(views[0]);
-  //     const loadedModel = await tcapi.viewer.getLoadedModel(annObjs.modelId);
-  //     const ifcBoltIds = views[0].ifcBoltIds;
-  //     var ifcBoltRuntimeIds = await tcapi.viewer.convertToObjectRuntimeIds(
-  //       modelId,
-  //       ifcBoltIds,
-  //     );
-  //     await tcapi.viewer.isolateEntities([
-  //       {
-  //         modelId: modelId,
-  //         entityIds: [asm, ...ifcBoltRuntimeIds],
-  //       },
-  //     ]);
-  //     // tcapi.viewer.setObjectState(
-  //     //   {
-  //     //     modelObjectIds: [
-  //     //       {
-  //     //         modelId: modelId,
-  //     //         objectRuntimeIds: ifcBoltRuntimeIds,
-  //     //       },
-  //     //     ],
-  //     //   },
-  //     //   {
-  //     //     visible: true,
-  //     //   },
-  //     // );
-
-  //     if (loadedModel === undefined) {
-  //       await tcapi.viewer.toggleModel(annObjs.modelId, true, false);
-  //       tcapi.viewer.setObjectState(
-  //         {
-  //           modelObjectIds: [
-  //             {
-  //               modelId: annObjs.modelId,
-  //             },
-  //           ],
-  //         },
-  //         {
-  //           visible: true,
-  //           color: {
-  //             r: 255,
-  //             g: 0,
-  //             b: 0,
-  //           },
-  //         },
-  //       );
-  //     } else {
-  //       tcapi.viewer.setObjectState(
-  //         {
-  //           modelObjectIds: [
-  //             {
-  //               modelId: annObjs.modelId,
-  //             },
-  //           ],
-  //         },
-  //         {
-  //           visible: true,
-  //           color: {
-  //             r: 255,
-  //             g: 0,
-  //             b: 0,
-  //           },
-  //         },
-  //       );
-  //     }
-  //     dispatch(ShowAnnRequest(false));
-  //   } while (annIds === undefined || annIds.length === 0);
+  //   return { ifcGuid, fileId };
   // }
 
-  // --- Utilities ---
+  // async function waitForModels(tcapi) {
+  //   let models;
+  //   do {
+  //     models = await tcapi.viewer.getModels();
+  //   } while (!models?.length);
+  //   return models;
+  // }
+
+  // async function waitForObjects(tcapi) {
+  //   let objects;
+  //   do {
+  //     objects = await tcapi.viewer.getObjects();
+  //   } while (!objects?.length);
+  //   return objects;
+  // }
+
+  // // --- Phase 1: Initialise ---
+
+  // async function initSession(tcapi) {
+  //   const token = await tcapi.extension.requestPermission("accesstoken");
+  //   window.localStorage.setItem("trimbleToken", token);
+  // }
+
+  // // --- Phase 2: Load model & isolate object ---
+
+  // async function loadAndIsolateModel(tcapi, ifcGuid) {
+  //   const models = await waitForModels(tcapi);
+
+  //   // Kick off annotation model look-up in parallel
+  //   for (const model of models) {
+  //     if (model.name.includes(".trb")) {
+  //       dispatch(GetAnnIdRequest({ name: model.name, modelId: model.id }));
+  //     }
+  //   }
+
+  //   await tcapi.viewer.reset();
+
+  //   for (const model of models) {
+  //     if (!model.name.includes(".ifc") && !model.name.includes(".tekla"))
+  //       continue;
+
+  //     const alreadyLoaded = await tcapi.viewer.getLoadedModel(model.id);
+  //     if (!alreadyLoaded) {
+  //       await tcapi.viewer.toggleModel(model.id, true, true);
+  //     }
+
+  //     await tcapi.viewer.setCamera("reset");
+  //     await waitForObjects(tcapi);
+
+  //     const runtimeIds = await tcapi.viewer.convertToObjectRuntimeIds(
+  //       model.id,
+  //       [ifcGuid],
+  //     );
+  //     if (runtimeIds?.length && runtimeIds[0] >= 0) {
+  //       return { modelId: model.id, runtimeId: runtimeIds[0] };
+  //     }
+  //   }
+
+  //   throw new Error(`No model found containing ifcGuid: ${ifcGuid}`);
+  // }
+
+  // async function selectAndFocus(tcapi, modelId, runtimeId) {
+  //   await tcapi.viewer.setSelection({
+  //     modelObjectIds: [{ modelId, objectRuntimeIds: [runtimeId] }],
+  //   });
+  //   await tcapi.viewer.isolateEntities([{ modelId, entityIds: [runtimeId] }]);
+  //   await tcapi.viewer.setCamera({
+  //     modelObjectIds: [{ modelId, objectRuntimeIds: [runtimeId] }],
+  //   });
+  // }
+
+  // // --- Phase 3: Render annotations ---
+
+  // async function renderAnnotations(tcapi, modelId, runtimeId, annIds, views) {
+  //   const annObjs = annIds.find((x) => x.name === views[0]?.file);
+  //   if (!annObjs)
+  //     throw new Error("No annotation object matches the current view");
+
+  //   const ifcBoltRuntimeIds = await tcapi.viewer.convertToObjectRuntimeIds(
+  //     modelId,
+  //     views[0].ifcBoltIds,
+  //   );
+
+  //   await tcapi.viewer.isolateEntities([
+  //     { modelId, entityIds: [runtimeId, ...ifcBoltRuntimeIds] },
+  //   ]);
+
+  //   const alreadyLoaded = await tcapi.viewer.getLoadedModel(annObjs.modelId);
+  //   if (!alreadyLoaded) {
+  //     await tcapi.viewer.toggleModel(annObjs.modelId, true, false);
+  //   }
+
+  //   tcapi.viewer.setObjectState(
+  //     { modelObjectIds: [{ modelId: annObjs.modelId }] },
+  //     { visible: true, color: { r: 255, g: 0, b: 0 } },
+  //   );
+
+  //   dispatch(ShowAnnRequest(false));
+  // }
+
+  // // --- Entry point ---
+
+  // async function fetchData(annIds, views) {
+  //   try {
+  //     const { ifcGuid, fileId } = parseUrlParams();
+
+  //     dispatch(GetDrawingRequest({ id: fileId }));
+
+  //     const tcapi = await WorkspaceAPI.connect(window.parent);
+  //     await initSession(tcapi);
+
+  //     const { modelId, runtimeId } = await loadAndIsolateModel(tcapi, ifcGuid);
+  //     setAsm(runtimeId);
+  //     setModelId(modelId);
+
+  //     await selectAndFocus(tcapi, modelId, runtimeId);
+  //     await renderAnnotations(tcapi, modelId, runtimeId, annIds, views);
+  //   } catch (err) {
+  //     console.error("[fetchData]", err);
+  //   }
+  // }
+  // React.useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  const tcapiRef = React.useRef(null);
+  const renderedAnnRef = React.useRef(false);
 
   function parseUrlParams() {
     const [, query] = window.location.href.split("?");
@@ -222,42 +179,48 @@ function App() {
 
     if (!ifcGuid || ifcGuid.length !== 22)
       throw new Error(`Invalid ifcGuid: "${ifcGuid}"`);
+
     if (!fileId) throw new Error("Missing fileId");
 
     return { ifcGuid, fileId };
   }
 
+  async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
   async function waitForModels(tcapi) {
-    let models;
-    do {
+    let models = [];
+
+    while (!models?.length) {
       models = await tcapi.viewer.getModels();
-    } while (!models?.length);
+      if (!models?.length) await sleep(300);
+    }
+
     return models;
   }
 
   async function waitForObjects(tcapi) {
-    let objects;
-    do {
+    let objects = [];
+
+    while (!objects?.length) {
       objects = await tcapi.viewer.getObjects();
-    } while (!objects?.length);
+      if (!objects?.length) await sleep(300);
+    }
+
     return objects;
   }
-
-  // --- Phase 1: Initialise ---
 
   async function initSession(tcapi) {
     const token = await tcapi.extension.requestPermission("accesstoken");
     window.localStorage.setItem("trimbleToken", token);
   }
 
-  // --- Phase 2: Load model & isolate object ---
-
   async function loadAndIsolateModel(tcapi, ifcGuid) {
     const models = await waitForModels(tcapi);
 
-    // Kick off annotation model look-up in parallel
     for (const model of models) {
-      if (model.name.includes(".trb")) {
+      if (model.name?.includes(".trb")) {
         dispatch(GetAnnIdRequest({ name: model.name, modelId: model.id }));
       }
     }
@@ -265,10 +228,14 @@ function App() {
     await tcapi.viewer.reset();
 
     for (const model of models) {
-      if (!model.name.includes(".ifc") && !model.name.includes(".tekla"))
+      const name = model.name?.toLowerCase() || "";
+
+      if (!name.includes(".ifc") && !name.includes(".tekla")) {
         continue;
+      }
 
       const alreadyLoaded = await tcapi.viewer.getLoadedModel(model.id);
+
       if (!alreadyLoaded) {
         await tcapi.viewer.toggleModel(model.id, true, true);
       }
@@ -280,8 +247,12 @@ function App() {
         model.id,
         [ifcGuid],
       );
+
       if (runtimeIds?.length && runtimeIds[0] >= 0) {
-        return { modelId: model.id, runtimeId: runtimeIds[0] };
+        return {
+          modelId: model.id,
+          runtimeId: runtimeIds[0],
+        };
       }
     }
 
@@ -290,69 +261,136 @@ function App() {
 
   async function selectAndFocus(tcapi, modelId, runtimeId) {
     await tcapi.viewer.setSelection({
-      modelObjectIds: [{ modelId, objectRuntimeIds: [runtimeId] }],
+      modelObjectIds: [
+        {
+          modelId,
+          objectRuntimeIds: [runtimeId],
+        },
+      ],
     });
-    await tcapi.viewer.isolateEntities([{ modelId, entityIds: [runtimeId] }]);
+
+    await tcapi.viewer.isolateEntities([
+      {
+        modelId,
+        entityIds: [runtimeId],
+      },
+    ]);
+
     await tcapi.viewer.setCamera({
-      modelObjectIds: [{ modelId, objectRuntimeIds: [runtimeId] }],
+      modelObjectIds: [
+        {
+          modelId,
+          objectRuntimeIds: [runtimeId],
+        },
+      ],
     });
   }
 
-  // --- Phase 3: Render annotations ---
-
   async function renderAnnotations(tcapi, modelId, runtimeId, annIds, views) {
-    const annObjs = annIds.find((x) => x.name === views[0]?.file);
-    if (!annObjs)
-      throw new Error("No annotation object matches the current view");
+    if (!annIds?.length) return;
+    if (!views?.length) return;
 
-    const ifcBoltRuntimeIds = await tcapi.viewer.convertToObjectRuntimeIds(
-      modelId,
-      views[0].ifcBoltIds,
-    );
+    const currentView = views[0];
+
+    const annObjs = annIds.find((x) => x.name === currentView?.file);
+
+    if (!annObjs) {
+      console.warn("No annotation object matches the current view");
+      return;
+    }
+
+    const ifcBoltRuntimeIds = currentView?.ifcBoltIds?.length
+      ? await tcapi.viewer.convertToObjectRuntimeIds(
+          modelId,
+          currentView.ifcBoltIds,
+        )
+      : [];
 
     await tcapi.viewer.isolateEntities([
-      { modelId, entityIds: [runtimeId, ...ifcBoltRuntimeIds] },
+      {
+        modelId,
+        entityIds: [runtimeId, ...ifcBoltRuntimeIds],
+      },
     ]);
 
     const alreadyLoaded = await tcapi.viewer.getLoadedModel(annObjs.modelId);
+
     if (!alreadyLoaded) {
       await tcapi.viewer.toggleModel(annObjs.modelId, true, false);
     }
 
-    tcapi.viewer.setObjectState(
-      { modelObjectIds: [{ modelId: annObjs.modelId }] },
-      { visible: true, color: { r: 255, g: 0, b: 0 } },
+    await tcapi.viewer.setObjectState(
+      {
+        modelObjectIds: [
+          {
+            modelId: annObjs.modelId,
+          },
+        ],
+      },
+      {
+        visible: true,
+        color: {
+          r: 255,
+          g: 0,
+          b: 0,
+        },
+      },
     );
 
     dispatch(ShowAnnRequest(false));
   }
 
-  // --- Entry point ---
-
-  async function fetchData(annIds, views) {
+  async function fetchData() {
     try {
       const { ifcGuid, fileId } = parseUrlParams();
 
       dispatch(GetDrawingRequest({ id: fileId }));
 
       const tcapi = await WorkspaceAPI.connect(window.parent);
+      tcapiRef.current = tcapi;
+
       await initSession(tcapi);
 
       const { modelId, runtimeId } = await loadAndIsolateModel(tcapi, ifcGuid);
+
       setAsm(runtimeId);
       setModelId(modelId);
 
       await selectAndFocus(tcapi, modelId, runtimeId);
-      await renderAnnotations(tcapi, modelId, runtimeId, annIds, views);
     } catch (err) {
       console.error("[fetchData]", err);
-      // TODO: dispatch an error action here so the UI can surface it
     }
   }
 
+  // Init chỉ chạy 1 lần
   React.useEffect(() => {
     fetchData();
   }, []);
+
+  // Đợi annIds + views có rồi mới render annotation
+  React.useEffect(() => {
+    async function run() {
+      if (renderedAnnRef.current) return;
+
+      if (!tcapiRef.current) return;
+      if (!modelId) return;
+      if (!asm) return;
+      if (!annIds?.length) return;
+      if (!views?.length) return;
+
+      renderedAnnRef.current = true;
+
+      try {
+        await renderAnnotations(tcapiRef.current, modelId, asm, annIds, views);
+      } catch (err) {
+        renderedAnnRef.current = false;
+        console.error("[renderAnnotations]", err);
+      }
+    }
+
+    run();
+  }, [annIds, views, modelId, asm]);
+
   return (
     <div className="App">
       <List>
